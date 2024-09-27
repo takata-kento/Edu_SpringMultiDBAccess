e34f27ddfcdc5a1c1a8f26e1494ff3ee40261e4a
takata-kento <takata_kento@ims-sol.co.jp>
2024-09-27 19:34:29 +0900

    APIの実装

---
+++ src/main/java/com/takata_kento/spring_multi_db_access/SpringMultiDbAccessApplication.java
@@ -0,0 +1,13 @@
+package com.takata_kento.spring_multi_db_access;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMultiDbAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMultiDbAccessApplication.class, args);
+	}
+
+}

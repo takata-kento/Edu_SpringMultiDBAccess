9515cdc79df89e8d493ec759ad9242e1f61e9cf3
takata-kento <takata_kento@ims-sol.co.jp>
2024-10-02 20:19:23 +0900

    JdbcTemplateの使用箇所をJdbcClientに変更

--- src/main/java/com/takata_kento/spring_multi_db_access/infrastructure/CustomerTransactionRepository.java
+++ src/main/java/com/takata_kento/spring_multi_db_access/infrastructure/CustomerTransactionRepository.java
@@ -4,60 +4,62 @@ import com.takata_kento.spring_multi_db_access.domain.Customer;
 import com.takata_kento.spring_multi_db_access.domain.CustomerTransactionRepositoryInterface;
 import com.takata_kento.spring_multi_db_access.domain.Transaction;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class CustomerTransactionRepository implements CustomerTransactionRepositoryInterface {
-    private final JdbcTemplate transactionDataJdbcTemplate;
-    private final JdbcTemplate customerDataJdbcTemplate;
+    private final JdbcClient transactionDataJdbcClient;
+    private final JdbcClient customerDataJdbcClient;
 
     public CustomerTransactionRepository(
-            @Qualifier("transactionDataJdbcTemplate") JdbcTemplate transactionDataJdbcTemplate,
-            @Qualifier("customerDataJdbcTemplate")    JdbcTemplate customerDataJdbcTemplate
+            @Qualifier("transactionDataJdbcClient") JdbcClient transactionDataJdbcClient,
+            @Qualifier("customerDataJdbcClient")    JdbcClient customerDataJdbcClient
     ) {
-        this.transactionDataJdbcTemplate = transactionDataJdbcTemplate;
-        this.customerDataJdbcTemplate = customerDataJdbcTemplate;
+        this.transactionDataJdbcClient = transactionDataJdbcClient;
+        this.customerDataJdbcClient = customerDataJdbcClient;
     }
 
     public Transaction getTransactionInfoById(int transactionId) {
-        return this.transactionDataJdbcTemplate.queryForObject(
-                """
+        return this.transactionDataJdbcClient
+                .sql("""
                     SELECT
                       log.transaction_id, log.customer_id, log.total, log.date
                     FROM
                       db1testdata.transaction_log as log
                     WHERE
-                      log.transaction_id = ?
-                """,
-                (rs, rowNum) -> new Transaction(
+                      log.transaction_id = :transaction_id
+                """)
+                .param("transaction_id", transactionId)
+                .query(
+                    (rs, rowNum) -> new Transaction(
                         rs.getInt("transaction_id"),
                         rs.getInt("customer_id"),
                         rs.getInt("total"),
-                        rs.getDate("date")
-                ),
-                transactionId
-        );
+                        rs.getDate("date"))
+                )
+                .single();
     }
 
     public Customer getCustomerInfoById(int customerId) {
-        return this.customerDataJdbcTemplate.queryForObject(
-                """
+        return this.customerDataJdbcClient
+                .sql("""
                     SELECT
                       customer.customer_id, customer.name, customer.age, customer.birthday, customer.gender
                     FROM
                       db2testdata.customer_info as customer
                     WHERE
-                      customer.customer_id = ?
-                """,
-                (rs, rowNum) -> new Customer(
+                      customer.customer_id = :customer_id
+                """)
+                .param("customer_id", customerId)
+                .query(
+                    (rs, rowNum) -> new Customer(
                         rs.getInt("customer_id"),
                         rs.getString("name"),
                         rs.getInt("age"),
                         rs.getDate("birthday"),
-                        rs.getString("gender")
-                ),
-                customerId
-        );
+                        rs.getString("gender"))
+                )
+                .single();
     }
 }

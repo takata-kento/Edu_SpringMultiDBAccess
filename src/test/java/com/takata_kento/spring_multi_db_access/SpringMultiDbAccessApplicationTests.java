673aa24bea1499716945f4dae78a5eb6620b17cb
takata-kento <takata_kento@ims-sol.co.jp>
2024-10-02 23:00:20 +0900

    不要なインポートの削除

--- src/test/java/com/takata_kento/spring_multi_db_access/SpringMultiDbAccessApplicationTests.java
+++ src/test/java/com/takata_kento/spring_multi_db_access/SpringMultiDbAccessApplicationTests.java
@@ -11,7 +11,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.jdbc.Sql;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;

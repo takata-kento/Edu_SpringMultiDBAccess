4bfe34f532f9937d7de11062bc4097c426633006
takata-kento <takata_kento@ims-sol.co.jp>
2024-10-09 09:28:03 +0900

    Qualifierアノテーションから合成アノテーションでのBean特定に変更

--- src/main/java/com/takata_kento/spring_multi_db_access/infrastructure/MultiDbAccessDataSourceConfiguration.java
+++ src/main/java/com/takata_kento/spring_multi_db_access/infrastructure/MultiDbAccessDataSourceConfiguration.java
@@ -1,12 +1,11 @@
 package com.takata_kento.spring_multi_db_access.infrastructure;
 
+import com.takata_kento.spring_multi_db_access.annotation.*;
 import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.JdbcClient;
 
 import javax.sql.DataSource;
@@ -15,42 +14,48 @@ import javax.sql.DataSource;
 public class MultiDbAccessDataSourceConfiguration {
     @Bean
     @ConfigurationProperties("app.datasource.transaction")
+    @TransactionDataSourceProperties
     public DataSourceProperties transactionDataSourceProperties() {
         return new DataSourceProperties();
     }
 
     @Bean
     @ConfigurationProperties("app.datasource.transaction.configuration")
+    @TransactionDataSource
     public HikariDataSource transactionDataSource(
-            @Qualifier("transactionDataSourceProperties") DataSourceProperties transactionDataSourceProperties
+            @TransactionDataSourceProperties DataSourceProperties transactionDataSourceProperties
     ) {
         return transactionDataSourceProperties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
     }
 
     @Bean
+    @TransactionDataJdbcClient
     JdbcClient transactionDataJdbcClient(
-            @Qualifier("transactionDataSource")DataSource dataSource
+            @TransactionDataSource DataSource dataSource
     ) {
         return JdbcClient.create(dataSource);
     }
 
     @Bean
     @ConfigurationProperties("app.datasource.customer")
+    @CustomerDataSourceProperties
     public DataSourceProperties customerDataSourceProperties() {
         return new DataSourceProperties();
     }
 
     @Bean
     @ConfigurationProperties("app.datasource.customer.configuration")
+    @CustomerDataSource
     public HikariDataSource customerDataSource(
-            @Qualifier("customerDataSourceProperties") DataSourceProperties customerDataSourceProperties
+            @CustomerDataSourceProperties DataSourceProperties customerDataSourceProperties
     ) {
         return customerDataSourceProperties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
     }
 
     @Bean
+    @CustomerDataJdbcClient
     JdbcClient customerDataJdbcClient(
-            @Qualifier("customerDataSource")DataSource dataSource
+            @CustomerDataSource DataSource dataSource
     ) {
         return JdbcClient.create(dataSource);
     }
